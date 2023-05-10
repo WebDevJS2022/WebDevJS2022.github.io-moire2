7355e4a025e3efdf1b263be834cd0384e920d357
WeDev <webdevjs2022@gmail.com>
2023-05-10 09:51:16 +0500

    3.3 Создание компонентов  ProductList.vue и ProductItem.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,6 @@
 <template>
  <section class="catalog">
-        <ul class="catalog__list">
-          <li class="catalog__item" v-for="(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image" :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-              <a href="#">
-                {{ product.title }}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{ product.price }} ₽
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label" for="input1">
-                  <input aria-labelledby="#input1" id="input1" class="colors__radio sr-only"
-                  type="radio" name="color-1" value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label" for="input2">
-                  <input aria-labelledby="#input2" id="input2" class="colors__radio sr-only"
-                  type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label" for="input3">
-                  <input aria-labelledby="#input3" id="input3" class="colors__radio sr-only"
-                  type="radio" name="color-1" value="#222">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </li>
-        </ul>
+  <ProductList :products="products"/>
 
         <ul class="catalog__pagination pagination">
           <li class="pagination__item">
@@ -98,9 +55,11 @@
 
 <script>
 import products from '@/data/products';
+import ProductList from '@/components/ProductList.vue';
 
 export default {
   name: 'App',
+  components: { ProductList },
   data() {
     return {
       products,
