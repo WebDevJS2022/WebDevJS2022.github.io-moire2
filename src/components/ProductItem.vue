0fb33d136e5548b9f2b170dcf50a462a2ae8d9f5
WeDev <webdevjs2022@gmail.com>
2023-05-10 10:30:36 +0500

    3.5 Атрибут key для списка = Переключение цвета для товара

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -18,8 +18,8 @@
             <li class="colors__item">
                 <label class="colors__label" for="input1">
                     <input aria-labelledby="#input1" id="input1"
-                    class="colors__radio sr-only" type="radio" name="color-1"
-                        value="#73B6EA" checked="">
+                    class="colors__radio sr-only" type="radio"
+                        value="#73B6EA" v-model="color">
                     <span class="colors__value" style="background-color: #73B6EA;">
                     </span>
                 </label>
@@ -27,8 +27,8 @@
             <li class="colors__item">
                 <label class="colors__label" for="input2">
                     <input aria-labelledby="#input2" id="input2"
-                    class="colors__radio sr-only" type="radio" name="color-1"
-                        value="#8BE000">
+                    class="colors__radio sr-only" type="radio"
+                        value="#8BE000" v-model="color">
                     <span class="colors__value" style="background-color: #8BE000;">
                     </span>
                 </label>
@@ -36,8 +36,8 @@
             <li class="colors__item">
                 <label class="colors__label" for="input3">
                     <input aria-labelledby="#input3" id="input3"
-                    class="colors__radio sr-only" type="radio" name="color-1"
-                        value="#222">
+                    class="colors__radio sr-only" type="radio"
+                        value="#222" v-model="color">
                     <span class="colors__value" style="background-color: #222;">
                     </span>
                 </label>
@@ -49,6 +49,11 @@
 <script>
 export default {
   name: 'ProductItem',
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
   props: ['product'],
 };
 </script>
