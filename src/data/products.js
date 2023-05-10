e2ae7b48218c1c65c1c0bccad6102c2e8cb89251
WeDev <webdevjs2022@gmail.com>
2023-05-10 09:03:38 +0500

    3.3 Создание компонента App, products.js

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Кружевной бюстгалтер без косточек',
+    price: 3690,
+    image: '/img/product-1.jpg',
+  },
+  {
+    title: 'Трусы с кружевными элементами',
+    price: 5660,
+    image: '/img/product-2.jpg',
+  },
+  {
+    title: 'Теплые шерстяные носки',
+    price: 790,
+    image: '/img/product-3.jpg',
+  },
+  {
+    title: 'Теплые шерстяные носки',
+    price: 690,
+    image: '/img/product-4.jpg',
+  },
+  {
+    title: 'Носки с принтом мороженое',
+    price: 960,
+    image: '/img/product-5.jpg',
+  },
+  {
+    title: 'Базовая футболка',
+    price: 960,
+    image: '/img/product-6.jpg',
+  },
+  {
+    title: 'Базовая хлопковая футболка',
+    price: 900,
+    image: '/img/product-7.jpg',
+  },
+  {
+    title: 'Базовая футболка для сна',
+    price: 600,
+    image: '/img/product-8.jpg',
+  },
+  {
+    title: 'Кружевной бюстгалтер на бретелях',
+    price: 4990,
+    image: '/img/product-9.jpg',
+  },
+  {
+    title: 'Мужские домашние шорты для сна',
+    price: 1090,
+    image: '/img/product-10.jpg',
+  },
+  {
+    title: 'Базовая хлопковая майка',
+    price: 560,
+    image: '/img/product-11.jpg',
+  },
+  {
+    title: 'Детский комбинезон (1-4 мес)',
+    price: 990,
+    image: '/img/product-12.jpg',
+  },
+];
