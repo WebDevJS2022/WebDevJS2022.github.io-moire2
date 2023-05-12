b6e927c74cf6eb7a388a807fbf3adf0f69199836
WeDev <webdevjs2022@gmail.com>
2023-05-12 08:10:26 +0500

    Установка Vuex

--- src/components/CartIndicator.vue
+++ src/components/CartIndicator.vue
@@ -3,7 +3,8 @@
           <svg width="19" height="24">
             <use xlink:href="#icon-cart"></use>
           </svg>
-          <span class="header__count" aria-label="Количество товаров">3</span>
+          <span class="header__count" aria-label="Количество товаров">
+            {{ $store.state.cartProducts.length }}</span>
     </router-link>
 </template>
 
